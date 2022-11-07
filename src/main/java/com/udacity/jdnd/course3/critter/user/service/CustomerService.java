0dd6ea8a547b8dfb989f5edd5de31a15c43bcddf
ibradi3 <ibrahimasory22@hotmail.com>
2022-11-07 23:51:30 +0100

    get owner by pet

--- src/main/java/com/udacity/jdnd/course3/critter/user/service/CustomerService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/service/CustomerService.java
@@ -3,6 +3,8 @@ package com.udacity.jdnd.course3.critter.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.udacity.jdnd.course3.critter.pet.entity.Pet;
+import com.udacity.jdnd.course3.critter.pet.service.PetService;
 import com.udacity.jdnd.course3.critter.user.entity.Customer;
 import com.udacity.jdnd.course3.critter.user.repository.CustomerRepository;
 
@@ -11,6 +13,9 @@ public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    PetService petService;
 
     // TODO Save customer
 
@@ -42,5 +47,14 @@ public class CustomerService {
     // TODO Delete all customers
 
     // TODO get all customer pets
+
+    // TODO get owner by pet
+    public Customer getOwnerByPet(Long petId) {
+        Pet pet = petService.getPetById(petId);
+        if (pet != null) {
+            return pet.getOwner();
+        }
+        return null;
+    }
 
 }
