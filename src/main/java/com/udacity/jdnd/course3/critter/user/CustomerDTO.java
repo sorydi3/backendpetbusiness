061c1b36b4d81d8c55cf0f3db92a0c7e4e8d4e04
ibradi3 <ibrahimasory22@hotmail.com>
2022-11-13 22:32:11 +0100

    init array list

--- src/main/java/com/udacity/jdnd/course3/critter/user/CustomerDTO.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/CustomerDTO.java
@@ -1,5 +1,6 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,7 +13,7 @@ public class CustomerDTO {
     private String name;
     private String phoneNumber;
     private String notes;
-    private List<Long> petIds;
+    private List<Long> petIds = new ArrayList<>();
 
     public long getId() {
         return id;
