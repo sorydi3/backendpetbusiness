1aa1e361e56c8ee1cbccf5750f4f393299738d24
ibradi3 <ibrahimasory22@hotmail.com>
2022-11-13 22:24:01 +0100

    added contructor

--- src/main/java/com/udacity/jdnd/course3/critter/skill/entity/Skill.java
+++ src/main/java/com/udacity/jdnd/course3/critter/skill/entity/Skill.java
@@ -1,24 +1,22 @@
 package com.udacity.jdnd.course3.critter.skill.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
-import com.udacity.jdnd.course3.critter.employeeskill.entity.EmployeeSkill;
 import com.udacity.jdnd.course3.critter.user.Skils;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class Skill {
 
     @Id
@@ -28,12 +26,13 @@ public class Skill {
     @Enumerated(EnumType.STRING)
     private Skils skillName;
 
-    @OneToMany(mappedBy = "skill")
-    private List<EmployeeSkill> employee;
-
     // constructor with killName
     public Skill(Skils skillName) {
         this.skillName = skillName;
+    }
+
+    // constructor
+    public Skill() {
     }
 
 }
