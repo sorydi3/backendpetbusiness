e4cbdda88b6d4c5982e1338a88bd373eb91c1477
ibradi3 <ibrahimasory22@hotmail.com>
2022-11-13 22:21:22 +0100

    Method : get employee by schills

--- src/main/java/com/udacity/jdnd/course3/critter/employeeskill/service/EmployeeSkillService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/employeeskill/service/EmployeeSkillService.java
@@ -1,10 +1,19 @@
 package com.udacity.jdnd.course3.critter.employeeskill.service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udacity.jdnd.course3.critter.employeeskill.entity.EmployeeSkill;
 import com.udacity.jdnd.course3.critter.employeeskill.repository.EmployeeSkillRepository;
+import com.udacity.jdnd.course3.critter.schedule.service.ScheduleService;
+import com.udacity.jdnd.course3.critter.skill.service.SkillService;
+import com.udacity.jdnd.course3.critter.user.Skils;
+import com.udacity.jdnd.course3.critter.user.entity.Employee;
 
 @Service
 public class EmployeeSkillService {
@@ -12,8 +21,21 @@ public class EmployeeSkillService {
     @Autowired
     EmployeeSkillRepository employeeSkillRepository;
 
+    @Autowired
+    SkillService skilService;
+
     // TODO: Add employee skill
     public EmployeeSkill saveEmployeeSkill(EmployeeSkill employeeSkill) {
         return employeeSkillRepository.save(employeeSkill);
+    }
+
+    public Set<Skils> getEmployeeSkills(Employee employee) {
+        List<EmployeeSkill> skills = employeeSkillRepository.findByEmployee(employee);
+        Set<Skils> skils = new HashSet<Skils>();
+        skills.forEach(skill -> {
+            skils.add(skill.getSkill().getSkillName());
+        });
+
+        return skils;
     }
 }
