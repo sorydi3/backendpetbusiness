72e124de013b05efe13e7485d4f52bf1b280615a
ibradi3 <ibrahimasory22@hotmail.com>
2022-11-28 23:26:55 +0100

    bug fix

--- src/main/java/com/udacity/jdnd/course3/critter/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/repository/ScheduleRepository.java
@@ -1,11 +1,11 @@
 package com.udacity.jdnd.course3.critter.schedule.repository;
 
-import java.time.DayOfWeek;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.udacity.jdnd.course3.critter.dayofweek.entity.Day;
 import com.udacity.jdnd.course3.critter.schedule.entity.Schedule;
 import com.udacity.jdnd.course3.critter.user.entity.Customer;
 import com.udacity.jdnd.course3.critter.user.entity.Employee;
@@ -15,7 +15,8 @@ public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
 
     List<Schedule> findByEmployee(Employee employee);
 
-    List<Schedule> findByDayOfWeek(DayOfWeek day);
+    List<Schedule> findByDayOfWeek(Day day);
 
     List<Schedule> findByCustomer(Customer customer);
+
 }
